c96b93826cc864e1fb88fe7c71b129c82ed2198e
StupidMoth <57850032+StupidMoth@users.noreply.github.com>
2019-12-24 23:29:58 +0300

    Add calcByFloydsAlgorithm()

--- calc.py
+++ calc.py
@@ -30,3 +30,28 @@ def calcTrafficMatrixIntensity(k, y, n):
             row.append(value)
         Y.append(row)
     return Y
+
+#Матрица кратчайших маршрутов между вершинами графа
+# Алгоритм Флойда
+# Принимает матрицу смежности как аргумент
+# Элемент матрицы смежности может быть None
+def calcByFloydsAlgorithm(R, n):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                R[i][j] = minNone(R[i][j], sumNone(R[i][k], R[k][j]))
+    return R
+
+sumNone = lambda a, b: a + b if (a != None and b != None) else None
+
+None2Number = lambda a: a if (a != None) else -1
+
+def minNone(a, b):
+    a = None2Number(a)
+    b = None2Number(b)
+    return min(a, b)    
+        
+
+
+
+    
